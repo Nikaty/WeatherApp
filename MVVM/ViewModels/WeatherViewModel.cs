b6d6c3e542652b3b4208a01651f4abb9580b75fa
Nikita Korolev <nmko.31@gmail.com>
2023-07-28 22:42:41 +0800

    gave some refactoring

--- MVVM/ViewModels/WeatherViewModel.cs
+++ MVVM/ViewModels/WeatherViewModel.cs
@@ -1,15 +1,6 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Input;
 using WeatherApp.MVVM.Models;
 using WeatherApp.MVVM.Services;
